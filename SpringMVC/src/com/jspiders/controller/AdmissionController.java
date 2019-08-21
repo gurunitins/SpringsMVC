f0b06a30ca054cc5e9bb6bc96804747a9af68288
GURU NITIN <GURU NITIN@DESKTOP-FKEADOS>
2019-08-21 08:20:56 +0530

    update

--- SpringMVC/src/com/jspiders/controller/AdmissionController.java
+++ SpringMVC/src/com/jspiders/controller/AdmissionController.java
@@ -1,6 +1,4 @@
 package com.jspiders.controller;
-
-
 import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
@@ -13,41 +11,19 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class AdmissionController {
 	
-
-//	jpa..hibarnate.springs. lamda framework   
-//	integration project
-	
-
 		@RequestMapping(value="/admissionForm", method= RequestMethod.GET )
 		public ModelAndView helloworld() {
-			ModelAndView mav=new ModelAndView("AdmissionForm");
-			
-			
-			
-			
+			ModelAndView mav=new ModelAndView("AdmissionForm");		
 			ArrayList<String> ar=new ArrayList<String>(); 
-		
-			
 			return mav;
 		}
-//		@RequestParam("StudentName") String name,@RequestParam("Studenthobby") String hobby
-		
-		
 		
 		@RequestMapping(value="/submitAdmissionForm", method= RequestMethod.POST)
 		public ModelAndView helloworld1(@ModelAttribute("std1") Student std) {
-			
 			ModelAndView mav=new ModelAndView("AdmissionSuccess");
-			
-			
-			mav.addObject("msg","details submitted by you" );
-					
+			mav.addObject("msg","details submitted by you" );		
 			return mav;
 		}
-		
-		
-		
-		
 	}
 		
 	
